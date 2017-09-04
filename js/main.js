83c724bbee1acf3d1d497761c70ae997e9e57df2
nikitas89 <cvnikita1@gmail.com>
2017-09-04 22:03:11 +0800

    created logic unit7

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 console.log("Up and running");
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+
+// var cards = ["queen", "queen", "king", "king"];
+// var cardsInPlay = [];
+//
+// let cardOne = cards[0];
+//
+ console.log("User Flipped " + cardOne);
+ console.log("User Flipped " + cardThree);
