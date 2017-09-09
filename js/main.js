db44e83f0572e2e9dfc389ae72cdb35c66805ba3
nikitas89 <cvnikita1@gmail.com>
2017-09-09 15:03:50 +0800

    unit 9 added functions

--- js/main.js
+++ js/main.js
@@ -3,23 +3,29 @@ console.log("Up and running");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+//fn that checks for match
+var checksForMatch = function(){
+  if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+      alert("You found a match!");
+      }else {
+        console.log("Sorry try again.");
+        alert("Sorry try again.");
+            }
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }else {
-      alert("Sorry try again.");
+      }
   }
 
-}
+//fn to store all steps that should happen when the user flips a card.
+var flipCard = function(cardId){
 
+cardsInPlay.push(cards[cardId]);
 
+checksForMatch();
 
- console.log("User Flipped " + cardOne);
- console.log("User Flipped " + cardTwo);
+console.log("User Flipped " + cards[cardId]);
+}// flipcard end
+
+flipCard(0);
+flipCard(1);
